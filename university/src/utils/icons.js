d1e90762313153903fe394df8ee79773085eea14
Mkhuphy <105905301+Mkhuphy@users.noreply.github.com>
2023-07-30 19:09:37 +0530

    Delete university directory

--- university/src/utils/icons.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
-export const iconsReact = {
-  left: <MdArrowBackIosNew />,
-  right: <MdArrowForwardIos />,
-};
